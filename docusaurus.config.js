18c4f917252830f37ccc629f9b041e95b19ad29f
Yevgeny Pats <yev.pats@gmail.com>
2021-08-07 20:21:43 +0300

    Update discord

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,28 +44,15 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              href: 'https://discord.gg/QwnWfrcfHg',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://twitter.com/cloudquery',
             },
           ],
         },
